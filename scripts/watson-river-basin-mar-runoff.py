c09d1ce66a30fbc60f0a3ccb841f53a39ae9b360
Maya Maciel-Seidman <maya.l.maciel-seidman@vanderbilt.edu>
2025-11-10 11:29:32 -0500

    get total daily runoff for the catchment

--- scripts/watson-river-basin-mar-runoff.py
+++ scripts/watson-river-basin-mar-runoff.py
@@ -11,6 +11,7 @@ import xarray as xr
 import rasterio
 import geopandas as gpd
 import rioxarray as rio
+import pandas as pd
 
 # Set working directory
 os.chdir('/Users/mayam/OneDrive/Documents/Duke University/ECS 851')
@@ -41,3 +42,35 @@ mar_wr = mar_wr.sel(TIME=slice("2024-05-01", "2024-09-01"))
 if os.path.exists('/Users/mayam/OneDrive/Documents/Duke University/ECS 851/SWOTson_River/mar_watson_river_clipped.nc'):
     os.remove('/Users/mayam/OneDrive/Documents/Duke University/ECS 851/SWOTson_River/mar_watson_river_clipped.nc')
 mar_wr.to_netcdf('/Users/mayam/OneDrive/Documents/Duke University/ECS 851/SWOTson_River/mar_watson_river_clipped.nc')
+
+# Get runoff for Watson River catchment
+runoff = mar_wr['RU']
+# Use Sector 0 for runoff
+runoff = runoff.isel(SECTOR1_1=0)
+
+# Compute area of each grid cell
+dx = np.abs(np.diff(mar_wr.x)[0])
+dy = np.abs(np.diff(mar_wr.y)[0])
+cell_area = dx * dy
+
+# Convert runoff from mmWE to m^3
+runoff_m = runoff / 1000.0
+# Calculate runoff volume per grid cell
+runoff_volume = runoff_m * cell_area
+
+# Sum runoff volume across all grid cells in the catchment
+total_runoff = runoff_volume.sum(dim=["y","x"])
+
+# Make a dataframe of dates and total runoff
+mar_runoff_df = pd.DataFrame({'DATE' : pd.to_datetime(mar_wr.TIME.values),
+                              'total_runoff': total_runoff.values})
+
+# Define output directory
+output_dir = '/Users/mayam/OneDrive/Documents/Duke University/ECS 851/SWOTson_River'
+# Define output file name with path
+ru_file_name = os.path.join(output_dir, "watson_river_runoff.csv")
+# Delete old csv file if it exists
+if os.path.exists(ru_file_name):
+    os.remove(ru_file_name)
+# Save runoff to csv
+mar_runoff_df.to_csv(ru_file_name, index=False)
